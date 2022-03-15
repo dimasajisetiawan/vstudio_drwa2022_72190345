754ee814833bdda9269ac3af8acd1ad895863abd
dimasajisetiawan <dimas.aji@si.ukdw.ac.id>
2022-03-15 17:31:43 +0700

    Add project files.

---
+++ Drwa_2022/Models/KelasItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Drwa_2022.Models
+{
+    public class KelasItem
+    {
+        private KelasContext context;
+
+        public int id { get; set; }
+        public string kelas { get; set; }
+
+        public string jurusan { get; set; }
+
+        public int sub { get; set; }
+    }
+}
